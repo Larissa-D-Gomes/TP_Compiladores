0095eb8ffc81a3ae29d17dcddd5ac129cf4aedb9
Lucas Bottrel Lopes de Moura <lucas.bottrel@levty.com>
2022-08-25 08:49:34 -0300

    initial documenting

---
+++ Lcompiler.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file Lcompiler.cpp
+ * 
+ * @authors Larissa Domingues Gomes, Lucas Bottrel Lopes de Moura e Vinicius Silva Mendes
+ * @brief A compiler for the L language, created for the compiler course of computation science at Puc Minas MG - BR
+ * @version 0.1
+ * @date 2022-08-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
